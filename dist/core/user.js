81a51451a77ad30d047ed747e69c4a284bb55e72
nulla2011 <nulla@outlook.com>
2022-08-21 15:08:16 +0800

    remove dist, npm 或 release 见

--- dist/core/user.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const USER_STAT_API = new URL('http://api.bilibili.com/x/space/upstat');
-const USER_RELATION_STAT_API = new URL('http://api.bilibili.com/x/relation/stat');
